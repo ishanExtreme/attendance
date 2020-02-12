0dac6905afa0fde244e12e7e14e04e80a82166e1
Ishan Mishra <ishan2198@hotmail.com>
2020-02-12 20:16:14 +0530

    ipinfo added

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-import logging
+# import logging
 # from requests import get
 from django.http import HttpResponse
-# import ipinfo
+import ipinfo
 
 from sheet.models import Sheet
 def home(request):
@@ -21,7 +21,7 @@ def reset(request):
 @login_required(login_url="/account/signup")
 def mark(request):
     # ip = get('https://api.ipify.org').text
-    logger = logging.getLogger(__name__)
+#     logger = logging.getLogger(__name__)
 
 #     ip =request.ipinfo.ip
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -32,14 +32,14 @@ def mark(request):
         ip = request.META.get('REMOTE_ADDR')
     
     
-    # access_token = 'f6ea795ded4276'
-    # handler = ipinfo.getHandler(access_token)
-    # details = handler.getDetails(ip)
+    access_token = 'f6ea795ded4276'
+    handler = ipinfo.getHandler(access_token)
+    details = handler.getDetails(ip)
     # print(details.latitude)
     # print(details.longitude)
     latitude = [20,21,22,23,24,25]
     longitude = [86,87,88,89,90,91,92,93]
-    if int((request.ipinfo.latitude)[0:(request.ipinfo.latitude).index('.')]) in latitude and int((request.ipinfo.longitude)[0:(request.ipinfo.longitude).index('.')]) in longitude:
+    if int((details.latitude)[0:(details.latitude).index('.')]) in latitude and int((details.longitude)[0:(details.longitude).index('.')]) in longitude:
         sheets = Sheet.objects.all()
         for sheet in sheets:
             if sheet.student == request.user.username:
@@ -47,7 +47,7 @@ def mark(request):
                 sheet.save()
         return redirect('home')
     else:
-        return render(request,'home/home.html',{'error':ip+" "+request.ipinfo.latitude+" "+request.ipinfo.longitude})
+        return render(request,'home/home.html',{'error':ip+" "+details.longitude+" "+details.longitude})
 
     
         
